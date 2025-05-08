0a64451e3790b94f3dea78ee1111f8d171f6ded6
Rahul Rudra <rahulrudra665@gmail.com>
2025-05-08 10:06:38 +0600

    changes made

--- ConceptualSesions/ConceptualClass-11(Mastering_With_DOM)/dom-b9-main/dom-b9-main/app.js
+++ ConceptualSesions/ConceptualClass-11(Mastering_With_DOM)/dom-b9-main/dom-b9-main/app.js
@@ -1,65 +1,95 @@
-let count = 0;
+// let count = 0;
+
+// const allBtn = document.getElementsByClassName("add-btn");
+
+// for (const btn of allBtn) {
+//   btn.addEventListener("click", function handleSelect(event) {
+//     count += 1;
+//     const budget = getValue("budget");
+
+//     const placeName = event.target.parentNode.childNodes[1].innerText;
+//     const price = event.target.parentNode.childNodes[3].childNodes[1].innerText;
+//     if (budget - parseInt(price) < 0) {
+//       alert("Low Budget Earn Moeny");
+//       return;
+//     }
+//     const selectedContainer = document.getElementById(
+//       "selected-place-container"
+//     );
+//     const li = document.createElement("li");
+//     const p = document.createElement("p");
+//     p.innerText = placeName;
+
+//     const p2 = document.createElement("p");
+//     p2.innerText = price;
+
+//     li.appendChild(p);
+//     li.appendChild(p2);
+//     selectedContainer.appendChild(li);
+//     totalPrice("total-cost", price);
+//     event.target.parentNode.parentNode.style.backgroundColor = "gray";
+//     event.target.setAttribute("disabled", true);
+//     setInnerText("budget", budget - parseInt(price));
+//     setInnerText("cart-count", count);
+//   });
+// }
+
+// function totalPrice(id, value) {
+//   const totalCost = document.getElementById("total-cost").innerText;
+//   const total = parseInt(totalCost) + parseInt(value);
+//   document.getElementById("total-cost").innerText = total;
+//   grandTotal("other");
+// }
+// function grandTotal(category) {
+//   console.log(category);
+//   const convertTotal = getValue("total-cost");
+//   if (category == "bus") {
+//     setInnerText("grand-total", convertTotal + 100);
+//   } else if (category == "train") {
+//     setInnerText("grand-total", convertTotal - 200);
+//   } else if (category == "flight") {
+//     setInnerText("grand-total", convertTotal + 500);
+//   } else {
+//     setInnerText("grand-total", convertTotal);
+//   }
+// }
+
+// function getValue(id) {
+//   const budgetInnerText = document.getElementById(id).innerText;
+//   const budget = parseInt(budgetInnerText);
+//   return budget;
+// }
+
+// function setInnerText(id, value) {
+//   document.getElementById(id).innerText = value;
+// }
 
 const allBtn = document.getElementsByClassName("add-btn");
-
+// console.log(allBtn);
+let count = 0;
 for (const btn of allBtn) {
-  btn.addEventListener("click", function handleSelect(event) {
-    count += 1;
-    const budget = getValue("budget");
+	btn.addEventListener("click", function (e) {
+		// console.log("Select Korechis");
+		count = count + 1;
+		console.log(e.target.parentNode.childNodes[1].innerText);
 
-    const placeName = event.target.parentNode.childNodes[1].innerText;
-    const price = event.target.parentNode.childNodes[3].childNodes[1].innerText;
-    if (budget - parseInt(price) < 0) {
-      alert("Low Budget Earn Moeny");
-      return;
-    }
-    const selectedContainer = document.getElementById(
-      "selected-place-container"
-    );
-    const li = document.createElement("li");
-    const p = document.createElement("p");
-    p.innerText = placeName;
+		const placeName = e.target.parentNode.childNodes[1].innerText;
+		const price = e.target.parentNode.childNodes[3].childNodes[1].innerText;
 
-    const p2 = document.createElement("p");
-    p2.innerText = price;
+		const selectedContainer = document.getElementById(
+			"selected-place-container"
+		);
 
-    li.appendChild(p);
-    li.appendChild(p2);
-    selectedContainer.appendChild(li);
-    totalPrice("total-cost", price);
-    event.target.parentNode.parentNode.style.backgroundColor = "gray";
-    event.target.setAttribute("disabled", true);
-    setInnerText("budget", budget - parseInt(price));
-    setInnerText("cart-count", count);
-  });
-}
-
-function totalPrice(id, value) {
-  const totalCost = document.getElementById("total-cost").innerText;
-  const total = parseInt(totalCost) + parseInt(value);
-  document.getElementById("total-cost").innerText = total;
-  grandTotal("other");
-}
-function grandTotal(category) {
-  console.log(category);
-  const convertTotal = getValue("total-cost");
-  if (category == "bus") {
-    setInnerText("grand-total", convertTotal + 100);
-  } else if (category == "train") {
-    setInnerText("grand-total", convertTotal - 200);
-  } else if (category == "flight") {
-    setInnerText("grand-total", convertTotal + 500);
-  } else {
-    setInnerText("grand-total", convertTotal);
-  }
-}
-
-function getValue(id) {
-  const budgetInnerText = document.getElementById(id).innerText;
-  const budget = parseInt(budgetInnerText);
-  return budget;
+		const li = document.createElement("li");
+		const p = document.createElement;
+		setInnerText("cart-count", count);
+		console.log(price, placeName);
+		// document.getElementById("cart-count").innerText = count;
+		// console.log(count);
+	});
+	// console.log(btn);
 }
 
 function setInnerText(id, value) {
-  document.getElementById(id).innerText = value;
+	document.getElementById(id).innerText = value;
 }
