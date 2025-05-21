37446d1dc4e19b7a73c34081bf08d38dba062066
Rahul Rudra <rahulrudra665@gmail.com>
2025-05-21 15:43:01 +0600

    changes made

--- Milestone-05/Module-27/Payoo-MFS-Resources-main/MyCode/home.js
+++ Milestone-05/Module-27/Payoo-MFS-Resources-main/MyCode/home.js
@@ -2,7 +2,7 @@ document
 	.getElementById("unique-add-money")
 	.addEventListener("click", (event) => {
 		event.preventDefault();
-		console.log("button clicked");
+		// console.log("button clicked");
 
 		const addMoneyInput = document.getElementById("add-money").value;
 
@@ -10,5 +10,19 @@ document
 
 		const pinInput = document.getElementById("pin-input").value;
 
-		console.log(pinInput);
+		// console.log(pinInput);
+
+		if (pinInput === "1234") {
+			console.log("adding money to your account");
+
+			const balance = document.getElementById("account-balance").innerText;
+
+			const addMoneyNumber = parseFloat(addMoneyInput);
+			const balanceNumber = parseInt(balance);
+			// console.log("22 number ", balanceNumber);
+			const newBalance = addMoneyNumber + balanceNumber;
+			// console.log(newBalance);
+		} else {
+			alert("Wrong Pin");
+		}
 	});
