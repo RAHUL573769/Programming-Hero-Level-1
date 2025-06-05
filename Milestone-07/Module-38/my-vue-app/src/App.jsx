8f869a32bbc09c89d5f4cd19f9e84f353bb39731
Rahul Rudra <rahulrudra665@gmail.com>
2025-06-05 08:10:07 +0600

    changes made

--- Milestone-07/Module-38/my-vue-app/src/App.jsx
+++ Milestone-07/Module-38/my-vue-app/src/App.jsx
@@ -1,23 +1,39 @@
 import "./App.css";
+import Todo from "./Todo";
 
 function App() {
 	return (
 		<>
 			<h1>Vite + React</h1>
-
-			<Person></Person>
+			<Todo task='Learn React'></Todo>
+			<Person person={{ name: "Rahul Rudra", size: 100 }}></Person>
+			<Developer name='Laptop'></Developer>
 		</>
 	);
 }
-
-function Person() {
+function Person(props) {
+	console.log(props);
+	const { name, size } = props.person;
 	const age = 25;
 	const money = 145;
 	return (
 		<h1 className='student'>
 			{" "}
-			I am Person with {age} with {money}
+			I am Person with {age} with {money} Name :{name} Size:{size}
 		</h1>
+	);
+}
+
+function Developer(props) {
+	const developerStyle = {
+		margin: "20px",
+		border: "2px solid red",
+	};
+	console.log(props);
+	return (
+		<div style={developerStyle}>
+			<h1>I am a Developer and have a {props.name}</h1>
+		</div>
 	);
 }
 
