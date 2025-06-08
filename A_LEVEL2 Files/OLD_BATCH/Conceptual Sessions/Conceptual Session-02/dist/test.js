7743bd33bb6c7f5b8bed9f9264230e21e7fafb4d
Rahul Rudra <rahulrudra665@gmail.com>
2025-06-08 08:24:38 +0600

    new commit

---
+++ A_LEVEL2 Files/OLD_BATCH/Conceptual Sessions/Conceptual Session-02/dist/test.js
@@ -0,0 +1,13 @@
+"use strict";
+let value = 120;
+let carName = "Tesla";
+let arr = [1, 3, 21, 34];
+let fruits = ["Apple", "Guava"];
+let employee = [1, "Apple", 2500];
+const friendsArray = ["Rahul"];
+const fruitsArray = ["Ear"];
+var PrintMedia;
+(function (PrintMedia) {
+})(PrintMedia || (PrintMedia = {}));
+let code;
+code = 123;
---
+++ A_LEVEL2 Files/OLD_BATCH/Conceptual Sessions/Conceptual Session-02/test.ts
@@ -0,0 +1,17 @@
+let value: number = 120;
+let carName: string = "Tesla";
+
+let arr: number[] = [1, 3, 21, 34];
+
+let fruits: string[] = ["Apple", "Guava"];
+
+let employee = [1, "Apple", 2500];
+
+const friendsArray: string[] = ["Rahul"];
+
+const fruitsArray: Array<string> = ["Ear"];
+
+enum PrintMedia {}
+
+let code: number | string;
+code = 123;
