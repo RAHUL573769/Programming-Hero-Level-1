345456e41a5d2091018a6483bd10e69afe3d2708
Rahul Rudra <rahulrudra665@gmail.com>
2025-07-04 08:02:14 +0600

    changes made

--- A_LEVEL2 Files/OLD_BATCH/Conceptual Sessions/Conceptual Session-03(Murir-Tin-Backend)/murir-tin-tours-and-travels-starter-pack/murir-tin-tours-and-travels-starter-pack/src/utils/catchAsync.ts
+++ A_LEVEL2 Files/OLD_BATCH/Conceptual Sessions/Conceptual Session-03(Murir-Tin-Backend)/murir-tin-tours-and-travels-starter-pack/murir-tin-tours-and-travels-starter-pack/src/utils/catchAsync.ts
@@ -1,8 +1,18 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, RequestHandler, Response } from 'express'
 
 export const catchAsync = (fn: RequestHandler) => {
   // return fn()
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((error: any) => {
+      next(error)
+    })
+  }
+}
+
+const catchAsyncNew1 = (fn: RequestHandler) => {
   return (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch((error: any) => {
       next(error)
