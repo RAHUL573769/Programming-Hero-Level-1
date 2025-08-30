95d56d7abe7e6a6efde4620b0269155c099bb513
Rahul Rudra <rahulrudra665@gmail.com>
2025-08-30 22:05:22 +0600

    changes made

--- A_LEVEL2 Files/OLD_BATCH/Conceptual Sessions/Mission-04/Reconnect With React/Eshadhin-starter-pack/Eshadhin-starter-pack/src/routes/index.tsx
+++ A_LEVEL2 Files/OLD_BATCH/Conceptual Sessions/Mission-04/Reconnect With React/Eshadhin-starter-pack/Eshadhin-starter-pack/src/routes/index.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
+import Cart from "../Cart";
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-    ],
-  },
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				index: true,
+				element: <Home />,
+			},
+			{ path: "/cart", element: <Cart></Cart> },
+		],
+	},
 ]);
